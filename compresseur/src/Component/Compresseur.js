b37a74ef89b0b527422a74147b5f4633dae49e7c
Adame93150 <56434201+Adame93150@users.noreply.github.com>
2021-10-18 14:48:31 +0200

    mise en place des dossiers

---
+++ compresseur/src/Component/Compresseur.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import React, {useState} from 'react';
+import Compressor from 'compressorjs';
+
+    
+
+const Compresseur = () => {
+  const [compressedFile, setCompressedFile] = useState(null);
+  
+  const handleCompressedUpload = (e) => {
+    const image = e.target.files[0];
+    new Compressor(image, {
+      quality: 0.8, // 0.6 can also be used, but its not recommended to go below.
+      success: (compressedResult) => {
+        // compressedResult has the compressed file.
+        // Use the compressed file to upload the images to your server.        
+        setCompressedFile(res)
+      },
+    });
+  };
+  
+  return (
+      <input
+        accept="image/*,capture=camera"
+        capture="”camera"
+        type="file"
+        onChange={(event) => this.handleCompressedUpload(event)}
+      />
+  );
+};
+
+export default Compresseur;
+
+
---
+++ compresseur/src/Pages/Accueil.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Compresseur from '../Component/Compresseur';
+
+const Accueil = () => {
+    return (
+        <div>
+            <Compresseur/>
+        </div>
+    );
+};
+
+export default Accueil;
