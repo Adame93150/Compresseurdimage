0645752d7f9ac6f72aa27efb72197eb47c06359a
Adame93150 <56434201+Adame93150@users.noreply.github.com>
2021-11-24 21:07:11 +0100

    image

--- compresseur/src/Component/Card.js
+++ compresseur/src/Component/Card.js
@@ -17,8 +17,8 @@ const CardX = () => {
 }
     return (
         
-<div class="row">
-  <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+<div class="placementimage">
+  
     <img
       src="https://fr.web.img6.acsta.net/c_310_420/pictures/210/038/21003859_20130507110423451.jpg"
     />
@@ -27,15 +27,13 @@ const CardX = () => {
       src="https://fr.web.img3.acsta.net/c_310_420/pictures/21/05/18/10/40/2487837.jpg"
     />
     
-  </div>
-  <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+  
+  
   <img
       src="https://fr.web.img3.acsta.net/c_310_420/pictures/21/05/18/10/40/2487837.jpg"
     />
-      <img
-      src="https://fr.web.img4.acsta.net/medias/nmedia/18/35/08/06/af.jpg"
-    />
-  </div>
+     
+  
   
 </div>
 
