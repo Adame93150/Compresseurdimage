556fddcf2e711d67ba3f607ae0c897484d953c4c
Adame93150 <56434201+Adame93150@users.noreply.github.com>
2021-11-24 21:39:31 +0100

    warnings 2

--- compresseur/src/Component/Card.js
+++ compresseur/src/Component/Card.js
@@ -10,17 +10,17 @@ const CardX = () => {
 <div class="placementimage">
   
     <img
-      src="https://fr.web.img6.acsta.net/c_310_420/pictures/210/038/21003859_20130507110423451.jpg"
+      src="https://fr.web.img6.acsta.net/c_310_420/pictures/210/038/21003859_20130507110423451.jpg" alt = "compressé numero 1"
     />
 
     <img
-      src="https://fr.web.img3.acsta.net/c_310_420/pictures/21/05/18/10/40/2487837.jpg"
+      src="https://fr.web.img3.acsta.net/c_310_420/pictures/21/05/18/10/40/2487837.jpg"  alt = "compressé numero 2"
     />
     
   
   
   <img
-      src="https://fr.web.img3.acsta.net/c_310_420/pictures/21/05/18/10/40/2487837.jpg"
+      src="https://fr.web.img3.acsta.net/c_310_420/pictures/21/05/18/10/40/2487837.jpg"  alt = "compressé numero 3"
     />
      
   
